6e045d5852106e840416edcbd1857286a430bc46
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-19 17:23:45 +0200

    Display card with url form in index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { UrlForm } from "@/components/url-form";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center max-w-lg mx-auto p-4"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center max-w-md mx-auto p-2">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Create link</CardTitle>
+          <CardDescription>
+            Create a short sharable link to any URL.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <UrlForm />
+        </CardContent>
+      </Card>
+    </main>
   );
 }
