cb7d903d357fb1ee4953d51fff5bf5069b3ef587
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-20 17:49:17 +0200

    Push new history item to front of array

--- lib/localstorage.ts
+++ lib/localstorage.ts
@@ -2,7 +2,7 @@ import { Redirect } from "./drizzle";
 
 export function addItemToLocalStorage(item: Redirect) {
   const items = JSON.parse(localStorage.getItem("items") || "[]");
-  items.push(item);
+  items.unshift(item);
   localStorage.setItem("items", JSON.stringify(items));
   window.dispatchEvent(new Event("storage"));
 }
