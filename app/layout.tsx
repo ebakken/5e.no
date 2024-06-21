5a184bba547768ca1fc47b8e5d278f497578fe5d
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-21 15:19:40 +0200

    Header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +24,32 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center justify-start max-w-md mx-auto px-4 py-8 space-y-8">
+          <header className="flex items-center justify-center w-full">
+            <Link href="/">
+              <Image src="/logo.png" alt="Logo" width={64} height={64} />
+            </Link>
+          </header>
           {children}
+          <footer className="flex items-center justify-center w-full px-2">
+            <p className="text-sm text-muted-foreground">
+              Built by{" "}
+              <Link
+                className="underline underline-offset-4 hover:text-foreground"
+                target="_blank"
+                href="https://github.com/ebakken"
+              >
+                ebakken
+              </Link>
+              . Source code available on{" "}
+              <Link
+                className="underline underline-offset-4 hover:text-foreground"
+                target="_blank"
+                href="https://github.com/ebakken/5e.no"
+              >
+                GitHub
+              </Link>
+            </p>
+          </footer>
         </main>
         <Toaster />
       </body>
