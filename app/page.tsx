c5d8c9daebf7c6de43990f71c3636ba248d97235
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-21 09:09:29 +0200

    Move <main /> to layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { UrlForm } from "@/components/url-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center max-w-md mx-auto px-4 py-8 space-y-8">
+    <div className="flex min-h-screen flex-col items-center justify-center max-w-md mx-auto px-4 py-8 space-y-8">
       <Card className="w-full">
         <CardHeader>
           <CardTitle>Create link</CardTitle>
@@ -23,6 +23,6 @@ export default function Home() {
         </CardContent>
       </Card>
       <History />
-    </main>
+    </div>
   );
 }
