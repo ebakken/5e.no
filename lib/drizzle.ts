250bbc8cdd17bbdd1d3b58304cf17f9908f49ac9
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-21 14:21:46 +0200

    Add contacts table to database schema

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -16,10 +16,26 @@ export const RedirectTable = pgTable("redirects", {
   createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
 
+export const ContactTable = pgTable("contacts", {
+  id: serial("id").primaryKey(),
+  slugId: text("slug_id")
+    .references(() => SlugTable.id)
+    .notNull(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  phone: text("phone").notNull(),
+  url: text("url"),
+  message: text("message"),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
+
 export type Slug = typeof SlugTable.$inferSelect;
 export type InsertSlug = typeof SlugTable.$inferInsert;
 
 export type Redirect = typeof RedirectTable.$inferSelect;
 export type InsertRedirect = typeof RedirectTable.$inferInsert;
+
+export type Contact = typeof ContactTable.$inferSelect;
+export type InsertContact = typeof ContactTable.$inferInsert;
 
 export const db = drizzle(sql);
