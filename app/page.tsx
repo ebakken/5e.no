088142564c457a60e5b88237acccaa824221ae89
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-21 12:17:54 +0200

    Add tab layout to switch forms

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,41 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { RedirectForm } from "@/components/redirect-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center max-w-md mx-auto px-4 py-8 space-y-8">
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Create link</CardTitle>
-          <CardDescription>
-            Create a short sharable link to any URL.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <RedirectForm />
-        </CardContent>
-      </Card>
+      <Tabs defaultValue="redirect" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="redirect">Link</TabsTrigger>
+          <TabsTrigger value="contact">Contact</TabsTrigger>
+        </TabsList>
+        <TabsContent value="redirect">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Create link</CardTitle>
+              <CardDescription>
+                Create a short sharable link to any URL.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <RedirectForm />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="contact">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Create contact card</CardTitle>
+              <CardDescription>
+                Create a contact card to share your contact information.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>Contact form</CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
       <History />
     </div>
   );
