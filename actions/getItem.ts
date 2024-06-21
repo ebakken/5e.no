94faa760649831cfdd7aa35c1ce0939672d03663
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-21 15:05:48 +0200

    Fetch contact when querying items

--- actions/getItem.ts
+++ actions/getItem.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { RedirectTable, SlugTable, db } from "@/lib/drizzle";
+import { ContactTable, RedirectTable, SlugTable, db } from "@/lib/drizzle";
 import { eq } from "drizzle-orm";
 
 export default async function getItem({ slugId }: { slugId: string }) {
@@ -9,6 +9,7 @@ export default async function getItem({ slugId }: { slugId: string }) {
     .from(SlugTable)
     .where(eq(SlugTable.id, slugId))
     .leftJoin(RedirectTable, eq(SlugTable.id, RedirectTable.slugId))
+    .leftJoin(ContactTable, eq(SlugTable.id, ContactTable.slugId))
     .then((rows) => rows[0]);
 
   return item;
