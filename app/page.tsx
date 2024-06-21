f056951996ac9a65a5b9f74c47090e1eba7ffa1c
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-21 14:24:23 +0200

    Make all element take up full width

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start max-w-md mx-auto px-4 py-8 space-y-8">
-      <Tabs defaultValue="redirect" className="w-[400px]">
+      <Tabs defaultValue="redirect" className="w-full">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="redirect">Link</TabsTrigger>
           <TabsTrigger value="contact">Contact</TabsTrigger>
@@ -35,7 +35,8 @@ export default function Home() {
             <CardHeader>
               <CardTitle>Create contact card</CardTitle>
               <CardDescription>
-                Create a contact card to share your contact information.
+                Create a contact card to easily share your contact information.
+                As of now, we only support norwegian contact information.
               </CardDescription>
             </CardHeader>
             <CardContent>Contact form</CardContent>
