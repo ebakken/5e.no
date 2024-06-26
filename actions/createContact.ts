24574fd4160d0f5adbe207f24a28f39ac883ea17
Edvard Bakken <edvard.bakken@gmail.com>
2024-06-26 16:15:15 +0200

    Send email notification on certain contact card registrations

--- actions/createContact.ts
+++ actions/createContact.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { db, ContactTable, SlugTable } from "@/lib/drizzle";
+import { transporter } from "@/lib/email";
 import { contactFormSchema } from "@/lib/schemas";
 import { nanoid } from "nanoid";
 import { z } from "zod";
@@ -44,6 +45,37 @@ export default async function createContact(
     .catch((error) => {
       return { database_error: error.message };
     });
+
+  if (
+    "contact" in transaction &&
+    process.env.EMAIL_RECEIPT_ADDRESS &&
+    process.env.EMAIL_REGISTRATION_FILTER &&
+    transaction.contact.email.includes(process.env.EMAIL_REGISTRATION_FILTER)
+  ) {
+    try {
+      const mail = await transporter.sendMail({
+        from: '"5e.no" <noreply@5e.no>',
+        to: process.env.EMAIL_RECEIPT_ADDRESS,
+        subject: "5e.no | Contact Creation Notification",
+        text: `A new contact has been created at https:\/\/5e.no/${
+          transaction.slug.id
+        } with the following details: ${JSON.stringify(transaction)}`,
+        html: `<p>A new contact has been created at <a href="https:\/\/5e.no/${
+          transaction.slug.id
+        }">https:\/\/5e.no/${
+          transaction.slug.id
+        }</a> with the following details:</p><pre>${JSON.stringify(
+          transaction,
+          null,
+          2
+        )}</pre>`,
+      });
+
+      console.log("Message sent: %s", mail.messageId);
+    } catch (error) {
+      console.error("Error occurred while sending email:", error);
+    }
+  }
 
   return transaction;
 }
